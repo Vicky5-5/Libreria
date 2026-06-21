52bbe6f69ff19941cd2f41bdb585e0dfd6500532
Vicky5_5 <vicgs99@gmail.com>
2026-06-21 21:52:38 +0200

    mejoraVisual Contacto

--- FrontEnd/FrontEnd_Libreria/src/app/Pages/Contacto/Contacto.ts
+++ FrontEnd/FrontEnd_Libreria/src/app/Pages/Contacto/Contacto.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatIconModule, MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: `./Contacto.html`,
   styleUrl: './Contacto.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
